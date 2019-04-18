c674e3b650c6fe5ee7f9d27acf6ee3b58aa3a226
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2019-04-18 16:56:03 -0500

    18 de Abril de 2019

--- GTK+/Calculadora/calculadora.c
+++ GTK+/Calculadora/calculadora.c
@@ -4,13 +4,15 @@
 void DetenerApp (GtkWidget *window, gpointer data);
 GtkWidget *AgregarBoton (GtkWidget *theBox, const gchar *buttonText, gpointer CallBackFunction, GtkWidget *EntryBox);
 void ClickNumero (GtkButton *button, gpointer data);
+void ClickC(GtkButton *button, gpointer data);
+void ClickMasMenos(GtkButton *button, gpointer data);
 
 gint main ( gint argc, gchar *argv[])
 {
 
   GtkWidget *ventana;
   GtkWidget *boton;
-  GtkWidget *cajah;
+  GtkWidget *cajah, *subcajah;
   GtkWidget *cajav;
   GtkWidget *cajae;
 
@@ -19,36 +21,47 @@ gint main ( gint argc, gchar *argv[])
   ventana = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_container_border_width(GTK_CONTAINER(ventana),5);
 
-  cajav = gtk_vbox_new(TRUE,5);
-  
+  cajav = gtk_vbox_new(TRUE,0);
+
   cajae = gtk_entry_new();
   gtk_box_pack_start(GTK_BOX(cajav),cajae,TRUE,TRUE,5);
-  
-  cajah = gtk_hbox_new(TRUE,5);
-  boton = AgregarBoton (cajah, "1", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "2", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "3", ClickNumero, cajae);
-  gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
 
   cajah = gtk_hbox_new(TRUE,5);
-  boton = AgregarBoton (cajah, "4", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "5", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "6", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "+", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "C", ClickC, cajae);
+  boton = AgregarBoton (cajah, "+/-", ClickMasMenos, cajae);
+  boton = AgregarBoton (cajah, "%", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "/", ClickNumero, cajae);
   gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
 
   cajah = gtk_hbox_new(TRUE,5);
   boton = AgregarBoton (cajah, "7", ClickNumero, cajae);
   boton = AgregarBoton (cajah, "8", ClickNumero, cajae);
   boton = AgregarBoton (cajah, "9", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "x", ClickNumero, cajae);
+  gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
+
+  cajah = gtk_hbox_new(TRUE,5);
+  boton = AgregarBoton (cajah, "4", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "5", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "6", ClickNumero, cajae);
   boton = AgregarBoton (cajah, "-", ClickNumero, cajae);
   gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
 
   cajah = gtk_hbox_new(TRUE,5);
-  boton = AgregarBoton (cajah, "0", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "/", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "*", ClickNumero, cajae);
-  boton = AgregarBoton (cajah, "=", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "1", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "2", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "3", ClickNumero, cajae);
+  boton = AgregarBoton (cajah, "+", ClickNumero, cajae);
+  gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
+
+  cajah = gtk_hbox_new(TRUE,5);
+  subcajah = gtk_hbox_new(TRUE,5);
+  boton = AgregarBoton (subcajah, "0", ClickNumero, cajae);
+  gtk_box_pack_start(GTK_BOX(cajah),subcajah,TRUE,TRUE,0);
+  subcajah = gtk_hbox_new(TRUE,5);
+  boton = AgregarBoton (subcajah, ".", ClickNumero, cajae);
+  boton = AgregarBoton (subcajah, "=", ClickNumero, cajae);
+  gtk_box_pack_start(GTK_BOX(cajah),subcajah,TRUE,TRUE,0);
   gtk_box_pack_start(GTK_BOX(cajav),cajah,TRUE,TRUE,5);
 
   gtk_signal_connect(GTK_OBJECT(ventana),"destroy",GTK_SIGNAL_FUNC(DetenerApp),NULL);
@@ -75,5 +88,18 @@ GtkWidget *AgregarBoton(GtkWidget *theBox, const gchar *buttonText, gpointer Cal
 
 void ClickNumero(GtkButton *button, gpointer data)
 {
-  gtk_entry_set_text(GTK_ENTRY(data),"Hola Mundo");
+  char Numero [3];
+  strcpy (Numero,gtk_button_get_label (button));
+  gtk_entry_append_text(GTK_ENTRY(data), Numero);
+}
+
+void ClickC(GtkButton *button, gpointer data)
+{
+  gtk_entry_set_text(GTK_ENTRY(data), "");
+}
+
+void ClickMasMenos(GtkButton *button, gpointer data)
+{
+  gtk_entry_prepend_text(GTK_ENTRY(data), "-(");
+  gtk_entry_append_text(GTK_ENTRY(data), ")");
 }
